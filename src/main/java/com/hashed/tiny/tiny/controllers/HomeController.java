03b088c29ffc6d704b3d95972d61aaa366129b7d
tagrohan <tagrohan@gmail.com>
2021-08-21 23:47:45 +0530

    TUrl method created

--- src/main/java/com/hashed/tiny/tiny/controllers/HomeController.java
+++ src/main/java/com/hashed/tiny/tiny/controllers/HomeController.java
@@ -1,18 +1,17 @@
 package com.hashed.tiny.tiny.controllers;
 
-import org.apache.tomcat.util.security.MD5Encoder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.Base64;
 
 @RestController
 public class HomeController {
 
 
-   @GetMapping("/testing")
-   public String testing() {
-      return "working";
+   @PostMapping("/get_url")
+   public String convert(@RequestParam("url") String originalUrl) {
+
+      // logic of conversion goes here
+      return "";
    }
 }
